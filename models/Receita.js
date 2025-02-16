4146c701f2b1d630e0ee3271378cb332983d3358
Kai0ult <kaioult00@gmail.com>
2025-02-16 15:49:12 -0300

    Criando pasta models e criando model de Receita

---
+++ models/Receita.js
@@ -0,0 +1,26 @@
+import banco from '../config/banco.js'
+
+const Receita = banco.sequelize.define('receitas', {
+    id:{
+        type: banco.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    descricao:{
+        type: banco.Sequelize.STRING(400),
+    },
+    valor:{
+        type: banco.Sequelize.FLOAT,
+    }
+    
+})
+
+Receita.belongsTo(Usuario, {
+    foreignKey: 'usuario_id',
+    constraint: true,
+    as: 'usuario'
+})
+
+Receita.sync()
+
+export default Receita
